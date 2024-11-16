1d259b79986ef10d938980c4c915ae4a57ae3954
ERETLY <68462812+ERETLY@users.noreply.github.com>
2024-11-16 16:23:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pickle
 import io
 import time
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -9,17 +10,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import psutil
+import pytz
 
-# Установка кодировки для вывода в консоль
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-# Функция для полной остановки всех процессов ChromeDriver
 def kill_chrome_driver_processes():
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == 'chromedriver' or 'chrome' in proc.info['name'].lower():
             proc.kill()
 
-# Настройки для headless-режима (заебали чета менять)
+# Настройки для headless-режима
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--disable-gpu")
@@ -38,7 +38,6 @@ chrome_options.add_argument("--allow-running-insecure-content")
 chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument("--disable-popup-blocking")
 
-# Функция для загрузки куки
 def load_cookies(driver, path):
     try:
         with open(path, 'rb') as cookiesfile:
@@ -49,128 +48,141 @@ def load_cookies(driver, path):
     except FileNotFoundError:
         print(f"File {path} not found, continuing without loading cookies.", flush=True)
 
-# Список файлов с куками - вписиывваете сами
+# Список файлов с куками - вписываете сами
 cookie_files = ['cookies.pkl', 'cookies1.pkl', 'cookies2.pkl']
-cookie_index = 0  # Индекс для текущего файла куки
 
-# Проверка на наличие карты и клик по карте
+# Массив для лимитов поиска карт
+Cards_for_cookies = [21, 20, 20]
+
 def check_for_card(driver, timeout):
-    start_time = time.time()  # Замер времени начала поиска карты
+    start_time = time.time()
     end_time = start_time + timeout
     card_found = False
 
     while time.time() < end_time:
-        print("Checking for card...", flush=True)  # Сообщение о проверке
+        print("Checking for card...", flush=True)
         try:
-            # Переключаемся на основной контент
-            driver.switch_to.default_content()  
-            
-            # Проверка на наличие элемента
+            driver.switch_to.default_content()
+            time.sleep(2)
             card_div = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, 'div.card-notification[data-card-name]'))
             )
-            # Если элемент найден, измеряем время и выводим сообщение
             time_taken = time.time() - start_time
-            print(f"\033[92mCard found! Time taken: {time_taken:.2f} seconds.\033[0m", flush=True)  # Зеленый цвет
+            print(f"\033[92mCard found! Time taken: {time_taken:.2f} seconds.\033[0m", flush=True)
             card_found = True
 
-            # Клик по карте через JavaScript
+            time.sleep(5)
             driver.execute_script("arguments[0].click();", card_div)
             print(f"\033[92mCard clicked.\033[0m", flush=True)
 
-            break  # Выходим из цикла
+            break
 
         except Exception as e:
             print("Card not found or not yet available, retrying...", flush=True)
-            time.sleep(10)  # Ждем 10 секунд перед повторной проверкой
+            time.sleep(10)
 
     if not card_found:
-        time_taken = time.time() - start_time  # Замер времени, даже если карта не найдена
-        print(f"\033[91mCard not found within the time limit. Total time spent: {time_taken:.2f} seconds.\033[0m", flush=True)  # Красный цвет
+        time_taken = time.time() - start_time
+        print(f"\033[91mCard not found within the time limit. Total time spent: {time_taken:.2f} seconds.\033[0m", flush=True)
     return card_found
 
-# Запуск бесконечного цикла для выполнения задачи
-while True:
-    # Полный перезапуск ChromeDriver
-    kill_chrome_driver_processes()
+def main():
+    cookie_index = 0
+    checks_per_cookie = {file: 0 for file in cookie_files}
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    reset_time = datetime.now(moscow_tz).replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
 
-    # Создаем новый экземпляр Service и инициализируем драйвер. Ставим свое расположение хромдрайвера
-    service = Service(executable_path=r'/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.get("https://animestars.org/aniserials/video/drama/1108-korzinka-fruktov-final.html")
+    while True:
+        current_time = datetime.now(moscow_tz)
+        
+        if current_time >= reset_time:
+            print("Resetting check counts at 00:00 MSK", flush=True)
+            checks_per_cookie = {file: 0 for file in cookie_files}
+            reset_time = current_time.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
 
-    # Загрузка куки из текущего файла
-    load_cookies(driver, cookie_files[cookie_index])
+        if checks_per_cookie[cookie_files[cookie_index]] >= Cards_for_cookies[cookie_index]:
+            cookie_index = (cookie_index + 1) % len(cookie_files)
+            if all(checks >= Cards_for_cookies[i] for i, checks in enumerate(checks_per_cookie.values())):
+                print("All cookie files have reached their limit. Waiting for reset...", flush=True)
+                time.sleep((reset_time - current_time).total_seconds())
+                continue
 
-    # Обновление страницы с загруженными куками
-    driver.refresh()
+        kill_chrome_driver_processes()
 
-    try:
-        # Ожидание появления iframe и переключение на него
-        print("Waiting for iframe to be available...", flush=True)
-        WebDriverWait(driver, 80).until(
-            EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, 'iframe[src*="kodik.info"]'))
-        )
-        print("Switched to iframe.", flush=True)
+        service = Service(executable_path=r'/usr/local/bin/chromedriver')
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.get("https://animestars.org/aniserials/video/drama/1108-korzinka-fruktov-final.html")
 
-        # Переменная для отслеживания наличия кнопки play
-        play_button_visible = True
+        load_cookies(driver, cookie_files[cookie_index])
 
-        while play_button_visible:
-            try:
-                # Поиск кнопки Play по XPath
-                play_button = WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/a'))
-                )
-                print("Play button found.", flush=True)
+        driver.refresh()
 
-                # Проверяем, виден ли элемент
-                if driver.execute_script("return arguments[0].offsetParent !== null;", play_button):
-                    # Нажимаем на кнопку через JavaScript
-                    driver.execute_script("arguments[0].click();", play_button)
-                    print("Click performed on the Play button.", flush=True)
+        driver.execute_script("window.scrollBy(0, 1190);")
+        print("Scrolled page to player", flush=True)
+        # time.sleep(2)
+        # driver.save_screenshot("card_found_screenshot.png")
 
-                    # Ожидание появления карточки в течение 600 секунд
-                    card_found = check_for_card(driver, 600)
+        try:
 
-                    # Перезагрузка страницы
-                    print("Reloading page...", flush=True)
-                    driver.refresh()  # Перезагрузка страницы
-                    print("Page reloaded successfully.", flush=True)
-                    time.sleep(10)  # Ждем 10 секунд перед новым запуском
-                    break  # Выходим из внутреннего цикла
+            print("Waiting for iframe to be available...", flush=True)
+            WebDriverWait(driver, 80).until(
+                EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, 'iframe[src*="kodik.info"]'))
+            )
+            print("Switched to iframe.", flush=True)
 
-                else:
+            play_button_visible = True
+
+            while play_button_visible:
+                try:
+                    play_button = WebDriverWait(driver, 30).until(
+                        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/a'))
+                    )
+                    print("Play button found.", flush=True)
+
+                    if driver.execute_script("return arguments[0].offsetParent !== null;", play_button):
+                        time.sleep(1)
+                        driver.execute_script("arguments[0].click();", play_button)
+                        print("Click performed on the Play button.", flush=True)
+
+                        # Ожидание появления карточки в течение 800 секунд
+                        card_found = check_for_card(driver, 800)
+
+                        if card_found:
+                            checks_per_cookie[cookie_files[cookie_index]] += 1
+                            print(f"Cards found for {cookie_files[cookie_index]}: {checks_per_cookie[cookie_files[cookie_index]]}/{Cards_for_cookies[cookie_index]}", flush=True)
+                        else:
+                            print(f"No card found for {cookie_files[cookie_index]}. Counter not incremented.", flush=True)
+
+                        print("Reloading page...", flush=True)
+                        driver.refresh()
+                        print("Page reloaded successfully.", flush=True)
+                        time.sleep(10)
+                        break
+
+                    else:
+                        play_button_visible = False
+                        print("Play button is no longer visible. Stopping clicks.", flush=True)
+
+                except Exception as e:
+                    if "stale element reference" in str(e):
+                        print("Stale element reference. Play button may have changed or been removed.", flush=True)
+                    elif "no such element" in str(e):
+                        print("Play button not found. Waiting for the next iteration.", flush=True)
+                    else:
+                        print("An error occurred while trying to find/click the button:", str(e), flush=True)
                     play_button_visible = False
-                    print("Play button is no longer visible. Stopping clicks.", flush=True)
 
-            except Exception as e:
-                # Обрабатываем исключение stale element или отсутствие элемента
-                if "stale element reference" in str(e):
-                    print("Stale element reference. Play button may have changed or been removed.", flush=True)
-                elif "no such element" in str(e):
-                    print("Play button not found. Waiting for the next iteration.", flush=True)
-                    play_button_visible = False
-                else:
-                    print("An error occurred while trying to find/click the button:", str(e), flush=True)
+        except Exception as e:
+            print("An error occurred:", str(e), flush=True)
 
-                # Останавливаем дальнейшие попытки
-                play_button_visible = False
+        finally:
+            driver.quit()
+            service.stop()
+            print("Restarting in 1 second.", flush=True)
 
-        # Переходим в режим ожидания перед перезапуском
-        print("Waiting for 10 seconds before restarting with new cookies.", flush=True)
-        time.sleep(10)  # Ждем 10 секунд
+        cookie_index = (cookie_index + 1) % len(cookie_files)
 
-    except Exception as e:
-        print("An error occurred:", str(e), flush=True)
+        time.sleep(1)
 
-    finally:
-        # Закрытие браузера и полная остановка службы
-        driver.quit()
-        service.stop()
-        print("Restarting in 1 seconds.", flush=True)
-
-    # Переключаем на следующий файл куки
-    cookie_index = (cookie_index + 1) % len(cookie_files)
-
-    time.sleep(1)
+if __name__ == "__main__":
+    main()
